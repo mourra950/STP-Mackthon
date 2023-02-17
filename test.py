44719648c86a3ab9ea3f5bee54fd2f7880d31f55
Omar <mourra24@gmail.com>
2023-02-17 16:47:22 +0200

    perspective

--- test.py
+++ test.py
@@ -7,7 +7,16 @@ import numpy as np
 
 from machathon_judge import Simulator, Judge
 
-c=False
+from simple_pid import PID
+
+pid = PID(1, 0.1, 0.01)
+
+def perspect_transform(img, src, dst):
+           
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
+        
+    return warped
 class FPSCounter:
     def __init__(self):
         self.frames = []
@@ -45,10 +54,11 @@ def thresholding(img, thresh=(200, 200, 200)):
 
 ###to get throttle
 def get_throttle(steering_angle) -> float:
-    if abs(steering_angle) < 0.070:
-        return 1.8
+    print(steering_angle)
+    if abs(steering_angle) < 0.030:
+        return 1.9,1.9
     else:
-        return 0.21
+        return 0.25,1
 
 
 
@@ -67,12 +77,29 @@ def run_car(simulator: Simulator) -> None:
         - get_state()
     """
     fps_counter.step()
-
+    dst_size= 40
+    bottom_offset= 0
+    source = np.float32([[222, 460],
+                         [377, 468],
+                         [376, 421],
+                         [252, 416],
+                         ])
+    
+    
     # Get the image and show it
     img = simulator.get_image()
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    
     fps = fps_counter.get_fps()
-
+    image=img
+    destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
+                [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],
+                [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], 
+                [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
+                ])
+    warped = perspect_transform(image, source, destination)
+    
+    # cv2.imwrite('./warped.png', warped)
     # draw fps on image
     # cv2.putText(
     #     img,
@@ -86,36 +113,50 @@ def run_car(simulator: Simulator) -> None:
     # )
    
     cv2.waitKey(1)
-
+   
     # Adham's edit 
     throttle = 0.17
     steering = 0
     steer_fact = 1.3
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    rgb = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB)
     bw = thresholding(rgb)
-    bw[0:300]=0
     kernel = np.ones((5,5),np.uint8)
-    bw = cv2.erode(bw,kernel,iterations = 7)
+    bw = cv2.erode(bw,kernel,iterations = 6)
+    bw[0:230]=0
+    
+    
+    ##
+    #mask
+    ##
+    # blank=np.ones_like(bw)
+    # maskrectangle1=cv2.rectangle(blank.copy(),(320-210,0),(320+210,480),(255,255,255),-1)
+    ################
+    ##
+    #apply mask
+    #
+    # bw=np.bitwise_and(bw,maskrectangle1)
+    ######
+    cv2.imshow("image", bw)
     
     xpix, ypix = rover_coords(bw[:,:,0])
     dists, angles = to_polar_coords(xpix, ypix)
     steering = np.mean(angles)*steer_fact
-    cv2.imshow("image", img)
-    # # Control the car using keyboard
-    steering = 0
-    if keyboard.is_pressed("a"):
-        steering = 1
-    elif keyboard.is_pressed("d"):
-        steering = -1
-
-    throttle = 0
-    if keyboard.is_pressed("w"):
-        throttle = 1
-    elif keyboard.is_pressed("s"):
-        throttle = 0
     
-    # throttle = get_throttle(steering * simulator.max_steer_angle / 1.7)
-    simulator.set_car_steering(steering * simulator.max_steer_angle / 1.7)
+    # # Control the car using keyboard
+    # steering = 0
+    # if keyboard.is_pressed("a"):
+    #     steering = 1
+    # elif keyboard.is_pressed("d"):
+    #     steering = -1
+
+    # throttle = 0
+    # if keyboard.is_pressed("w"):
+    #     throttle = 1
+    # elif keyboard.is_pressed("s"):
+    #     throttle = 0
+    
+    throttle,fact= get_throttle(steering * simulator.max_steer_angle / 1.7)
+    simulator.set_car_steering(steering * simulator.max_steer_angle /fact)
     simulator.set_car_velocity(throttle * 25)
 
 
